7c5756f9a33ddb20675b237bf55c275a806d28e0
U-BSD\465151 <465151@SHS-GYBF6X3.beaverton.k12.or.us>
2026-09-08 09:58:51 -0700

    Checks for a palindrome.

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,3 +1,7 @@
+/*
+Elijah Chan 9/8/2026
+Reads in a series of characters up to 80 characters long and returns if it is a palindrome or not.
+*/
 #include <iostream>
 #include <cctype>
 
@@ -9,10 +13,12 @@ int main()
   char strip_inp[80];
   int count = 0;
   bool palindrome = true;
-  
+
+  //Takes the user's inputted characters and puts them into the input char list.
   cout << "Enter your text: ";
   cin.get(input, 80);
 
+  //Goes through every character in the input and adds a lowercase version to the strip_inp char list if it is an alphanumeric character.
   //https://www.geeksforgeeks.org/c/isalnum-function-c-language/
   //https://www.geeksforgeeks.org/cpp/tolower-function-in-cpp/
   for (int i = 0; i < 80; i++) {
@@ -30,6 +36,7 @@ int main()
     }
   }
 
+  //Checks through the strip_inp char list and makes palindrome false if it isn't a palindrome.
   for (int i = 0; i < count / 2; i++) {
     if (strip_inp[i] != strip_inp[count - i]) {
       palindrome = false;
@@ -37,6 +44,7 @@ int main()
     }
   }
 
+  //Outputs if the input was a palindrome or not.
   if (palindrome) {
     cout << "Palindrome.";
   }
