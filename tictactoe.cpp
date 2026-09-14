42d9df859372dfb966f6b060732054b6ac089ca0
U-BSD\465151 <465151@SHS-GYBF6X3.beaverton.k12.or.us>
2026-09-14 10:24:44 -0700

    push

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,14 +6,60 @@ Elijah Chan 9/8/2026
 
 using namespace std;
 
-void init(char* board)
+void printBoard(char** board, char* lett)
 {
-  board[0][1] = '1'; 
-}
-int main()
-{
-  char board[6][6];
-  char* boardptr = board;
+  cout << " 123" << endl;
+  for(int i = 0; i < 3; i++) {
+    char outl[4];
+    outl[0] = lett[i];
+    for(int j = 0; j < 3; j++) {
+      outl[j + 1] = board[i][j];
+    }
+    cout << outl << endl;
+  }
   
 }
 
+bool checkWin(char** board) {
+  return false;
+}
+
+
+void place(char** board, char coord[2], bool player) {
+  if(player) {
+    board[coord[0]][coord[1]] = 'x';
+  }
+  else {
+    board[coord[0]][coord[1]] = 'o';
+  }
+}
+
+int main()
+{
+  char lett[3] = {'a', 'b', 'c'};
+  char* lettptr = lett;
+  char* board[3];
+  char** boardptr = board;
+  bool playing = true;
+  char input[2];
+  bool player = true;
+  
+  for(int i = 0; i < 3; i++) {
+    char* b = new char[3];
+    for(int j = 0; j < 3; j++) {
+      b[j] = ' ';
+    }
+    board[i] = b;
+  }
+
+  printBoard(boardptr, lettptr);
+  
+  while(playing) {
+    cout << "Enter a coordinate point: ";
+    cin >> input;
+    cout << input << endl;
+    printBoard(boardptr, lettptr);
+    playing = false;
+  }
+  
+}
