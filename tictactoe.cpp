37153a50b7bf58f9b133d56644ea91fe7434844e
U-BSD\465151 <465151@SHS-GYBF6X3.beaverton.k12.or.us>
2026-09-16 10:23:16 -0700

    tasds

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,6 +6,11 @@ Elijah Chan 9/8/2026
 
 using namespace std;
 
+void clearBoard(char** board) {
+  return;
+}
+  
+
 void printBoard(char** board, char* lett)
 {
   cout << " 123" << endl;
@@ -13,27 +18,38 @@ void printBoard(char** board, char* lett)
     char outl[4];
     outl[0] = lett[i];
     for(int j = 0; j < 3; j++) {
-      outl[j + 1] = board[i][j];
+      outl[1 + j] = board[i][j];
     }
     cout << outl << endl;
   }
   
 }
 
-bool checkWin(char** board) {
-  return false;
+int checkWin(char** board) {
+  return 0;
 }
 
 
-void place(char** board, char coord[2], bool player) {
-  if(player) {
-    board[coord[0]][coord[1]] = 'x';
-  }
+int place(char** board, int coords[2], bool* playerptr) {
+  bool player = playerptr[0];
+  if(board[coords[0]][coords[1]] == ' ') {
+    if(player) {
+      board[coords[0]][coords[1]] = 'x';
+    }
+    else {
+      board[coords[0]][coords[1]] = 'o';
+    }
+    player = !player;
+    }
   else {
-    board[coord[0]][coord[1]] = 'o';
+    cout << "Invalid placement";
   }
+  int win = checkWin(board);
+  if(checkWin(board)) {
+    return 0;
+  }
+  return 0;
 }
-
 int main()
 {
   char lett[3] = {'a', 'b', 'c'};
@@ -42,7 +58,9 @@ int main()
   char** boardptr = board;
   bool playing = true;
   char input[2];
+  int coords[2];
   bool player = true;
+  int win = 0;
   
   for(int i = 0; i < 3; i++) {
     char* b = new char[3];
@@ -51,13 +69,27 @@ int main()
     }
     board[i] = b;
   }
-
+  
   printBoard(boardptr, lettptr);
   
   while(playing) {
     cout << "Enter a coordinate point: ";
-    cin >> input;
-    cout << input << endl;
+    cin.get(input, 2);
+    cin.get();
+    cout << input;
+    
+    if(input[0] == 'a') {
+      coords[0] = 0;
+    }
+    else if(input[0] == 'b') {
+      coords[0] = 1;
+    }
+    else if(input[0] == 'c') {
+      coords[0] = 2;
+    }
+    coords[1] = input[1];
+    cout << coords[0] << coords[1] << endl;
+    win = place(boardptr, coords, &player);
     printBoard(boardptr, lettptr);
     playing = false;
   }
